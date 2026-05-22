eb3940c7777d3a54eeafd19c2a1cf1aadeea878d
Vitor <vitorampudia@gmail.com>
2026-05-21 21:45:40 -0300

    começo lista livros

---
+++ src/components/LivroCard.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+export default {
+    name: 'LivroCard',
+    props: {
+        livro: Object
+    },
+    computed: {
+        
+    },
+    methods:{
+        detalhesLivro(){
+
+        }
+    }
+}
+</script>
+<template>
+    <b-card
+    :title=""
+    class="text-center mb-3"
+    >
+        <b-img
+            :src="321"
+
+        />
+        <b-button variant="primary" class="mt-1" @click="detalhesLivro()">
+            Ver Detalhes
+        </b-button>
+
+    </b-card>
+</template>
---
+++ src/views/ListaLivrosView.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import axios from 'axios';
+import LivroCard from '@/components//LivroCard.vue'
+export default{
+    name: 'ListaLivrosView',
+    components: {
+        LivroCard
+    },
+    data() {
+        return {
+            next: "",
+
+        }
+    }
+}
+</script>
+<template>
+    <LivroCard
+    
+    ></LivroCard>
+</template>
