7661642e8601e015a893979284130412b534119d
Vitor Ampudia <vitorampudia@gmail.com>
2026-05-27 22:20:07 -0300

    Desenvolvimento da listagem e do CardLivro

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import BootstrapVue from 'bootstrap-vue';
 
+// // Importe os arquivos CSS do Bootstrap e BootstrapVue
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import BootstrapVue3 from 'bootstrap-vue-3'
 const app = createApp(App)
+
+app.use(BootstrapVue3)
 
 app.use(router)
 
--- src/views/ListaLivrosView.vue
+++ src/views/ListaLivrosView.vue
@@ -9,13 +9,39 @@ export default{
     data() {
         return {
             next: "",
+            total: null,
+            livros: null,
 
         }
+    },
+    methods:{
+        async listarLivros() {
+            const api = axios.create({
+                baseURL:`https://gutendex.com/books/`
+            })
+
+            try {
+                let resposta = await api.get("")
+                console.log(resposta)
+                this.livros = resposta.data.results
+                this.total = resposta.data.count
+            } catch (erro) {
+                console.log(erro)
+            }
+        }
+    },
+    mounted(){
+        this.listarLivros()
+        console.log("Teste")
     }
 }
 </script>
 <template>
-    <LivroCard
-    
-    ></LivroCard>
+    <div class="d-flex container align-items-center flex-column">
+        <b-row>
+            <b-col class="mb-3" v-for="livro in livros" :key="livro.name">
+                <LivroCard :livro="livro"/>
+            </b-col>
+        </b-row>
+    </div>
 </template>
