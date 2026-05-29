3a28dd66b4d05c641042e679ad143d98606b5ace
Vitor <vitorampudia@gmail.com>
2026-05-28 21:00:29 -0300

    Produzindo a listagem. Falta centralização da grid e compor o card com infos

--- src/components/LivroCard.vue
+++ src/components/LivroCard.vue
@@ -1,37 +1,44 @@
 <script lang="ts">
 export default {
-    name: 'LivroCard',
-    props: {
-        livro: Object
-    },
-    data(){
-        return {
-            titulo: this.livro?.title,
-            autor: this.livro?.authors,
-            capa: this.livro?.formats["image/jpeg"],
-            lang: this.livro?.languages[0]
-        }
-    },
-    computed: {
-        
-    },
-    methods:{
-        detalhesLivro(){
-
-        },
+  name: 'LivroCard',
+  props: {
+    livro: Object,
+  },
+  data() {
+    return {
+      titulo: this.livro?.title,
+      autor: this.livro?.authors[0]?.name ?? 'Desconhecido',
+      capa: this.livro?.formats['image/jpeg'],
+      lang: this.livro?.languages[0],
+      subject: this.livro?.subjects,
     }
+  },
+  methods: {
+    detalhesLivro() {},
+  },
 }
 </script>
+
 <template>
-    <b-card-group deck>
-        <b-card :title="titulo" :img-src="capa" img-alt="Livro" width="50px" img-top>
-            <b-card-text>
-                This is a wider card with supporting text below as a natural lead-in to additional content.
-                This content is a little bit longer.
-            </b-card-text>
-            <template #footer>
-                <small class="text-muted">Language: {{ lang }}</small>
-            </template>
-        </b-card>
-    </b-card-group>
+  <b-card
+    :img-src="capa"
+    img-top
+    img-class="img-livro"
+    class="card-livro shadow-sm d-flex flex-column"
+  >
+    <h1>teste</h1>
+    
+  </b-card>
 </template>
+
+<style>
+    .card-livro{
+        width: 100%;
+        max-width: 220px;
+        margin: auto;
+    }
+    .card-img-top{
+        height: 250px;
+        object-fit: cover;
+    }
+</style>
--- src/views/ListaLivrosView.vue
+++ src/views/ListaLivrosView.vue
@@ -1,44 +1,48 @@
 <script lang="ts">
-import axios from 'axios';
-import LivroCard from '@/components//LivroCard.vue'
-export default{
-    name: 'ListaLivrosView',
-    components: {
-        LivroCard
-    },
-    data() {
-        return {
-            next: "",
-        }
-    },
-    methods:{
-        async listarLivros() {
-            const api = axios.create({
-                baseURL:`https://gutendex.com/books/`
-            })
+import axios from 'axios'
+import LivroCard from '@/components/LivroCard.vue'
 
-            try {
-                let resposta = await api.get("")
-                console.log(resposta)
-                this.livros = resposta.data.results
-                this.total = resposta.data.count
-            } catch (erro) {
-                console.log(erro)
-            }
-        }
-    },
-    mounted(){
-        this.listarLivros()
-        console.log("Teste")
+export default {
+  name: 'ListaLivrosView',
+  components: { LivroCard },
+  data() {
+    return {
+      next: '',
+      total: null,
+      livros: null,
     }
+  },
+  methods: {
+    async listarLivros() {
+      const api = axios.create({ baseURL: 'https://gutendex.com/books/' })
+      try {
+        const resposta = await api.get('')
+        this.livros = resposta.data.results
+        this.total = resposta.data.count
+      } catch (erro) {
+        console.error(erro)
+      }
+    },
+  },
+  mounted() {
+    this.listarLivros()
+  },
 }
 </script>
+
 <template>
-    <div class="d-flex container align-items-center flex-column">
-        <b-row>
-            <b-col class="mb-3" v-for="livro in livros" :key="livro.name">
+  < <div class="container mt-4 d-flex flex-column align-items-center">
+        <b-row class="g-3 justify-content-center">
+            <b-col
+                v-for="livro in livros"
+                :key="livro.id"
+            >
                 <LivroCard :livro="livro"/>
             </b-col>
         </b-row>
     </div>
+    
 </template>
+
+<style scoped>
+</style>
